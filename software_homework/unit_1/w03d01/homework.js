1ac272cb8fdce2735841ec0cb0ea005146f774ad
tinakw <tk.worden@gmail.com>
2022-08-10 20:45:29 -0400

    Created Person Class

--- software_homework/unit_1/w03d01/homework.js
+++ software_homework/unit_1/w03d01/homework.js
@@ -1,9 +1,29 @@
-// class Hamster
+// ///////////////////////
+//class Hamster
+//////////////////////////
+// Hamster
+// attributes:
+
+// owner - string, initially set as an empty string
+// name - string, set the name from parameter in constructor method
+// price - integer, set as 15
+// methods:
+
+// wheelRun() - log "squeak squeak"
+// eatFood() - log "nibble nibble"
+// getPrice() - return the price
+// 🔴 Hard Mode Save & Commit your work!
+// Your commit message should read something like:
+// "created hamster class"
+// Easy Mode Make sure it works so far
+//......................................................
+
+
 class Hamster {
-    constructor (owner, name, price){
-        this.nameOfOwner = owner;
-        this.nameofHamster = name;
-        this.price = price;
+    constructor (){
+        this.nameOfOwner = "none";
+        this.nameofHamster = "none";
+        this.price = 15;
     }
 
     wheelRun(){
@@ -15,7 +35,6 @@ class Hamster {
     }
 
     getPrice() {
-        this.price = 15;
         return this.price;
     }
 };
@@ -24,16 +43,63 @@ class Hamster {
 //  console.log(Gus);
 //  console.log(Gus.eatFood());
 
+//////////////////////////////////////////////////////
 // class Person
+//////////////////////////////////////////////////////
+
+// Person
+// attributes:
+
+// name - set name from parameter in constructor method
+// age - initially 0
+// height - initially 0
+// weight - initially 0
+// mood - integer starting at 0 initially
+// hamsters - empty array initially
+// bankAccount - initially set to 0
+// methods:
+
+// getName() - returns name
+// getAge() - returns age
+// getWeight() - returns weight
+// greet() - logs a message with person's name
+// eat() - increment weight, increment mood
+// exercise() - decrement weight
+// ageUp() - increment age, increment height, increment weight, decrement mood, increment bank account by 10 (birthday money)
+// buyHamster(hamster) - push the hamster object onto the hamster array, increment mood by 10, decrement bankAccount by the value of the hamster (hint: use getPrice())
+// 🔴 Hard Mode Save & Commit your work!
+// Your commit message should read something like:
+// "created person class"
+// Easy Mode Keep Going and save and run code
+/////..................................................
+//
+///Create a Story with your Person class
+// Feel free to update or add methods to automate some of these tasks.
+
+// Instantiate a new Person named Timmy
+// Age Timmy five years
+// At this point Timmy's a little bummed. As a precocious child, he feels he's "seen it all" already. Have him eat five times.
+// Now Timmy's a little heavier than he wants to be. Kindergarten's coming up and he wants to look good. Have him exercise five times
+// Age Timmy 9 years
+// Create a hamster named "Gus"
+// Set Gus's owner to the string "Timmy"
+// Have Timmy "buy" Gus
+// Age Timmy 15 years
+// Have Timmy eat twice
+// Have Timmy exercise twice
+// 🔴 Hard Mode Save & Commit your work!
+// Your commit message should read something like:
+// "created timmys story" Easy Mode Keep Going Save and Run Code
+/////...................................................
 
 class Person {
-    constructor (name, age, height, weight, mood, hamsters, bankAccount){
+    constructor (name, age, height, weight, mood, bankAccount){
         this.name = name;
         this.age = age;
         this.height = height;
         this.weight = weight;
         this.mood = mood;
-        this.hamsters = hamsters;
+        this.hamsters = [];
         this.bankAccount = bankAccount;  
         }
 
@@ -42,7 +108,7 @@ class Person {
         }
 
         getAge(){
-            return this.age;
+            return `Timmy is ` + this.age + ` years old!`;
         }
 
         getWeight(){
@@ -54,24 +120,69 @@ class Person {
         }
 
         eat(num){
-            return `Timmy is ` + this.age + ` and his mood is ` + this.mood + ` and he currently weighs ` + this.weight + `. He eats ` + num + ` times! Eating increases mood to ` + (this.mood+=num) + ` and weight to ` + (this.weight+=(num*5)) + `!`;
+            return this.name + ` is ` + this.age + ` and his mood is ` + this.mood + ` and he currently weighs ` + this.weight + `. He eats ` + num + ` times! Eating increases mood to ` + (this.mood+=num) + ` and weight to ` + (this.weight+=(num*5)) + `!`;
         }
 
-        exercise(){
-            return `Exercise reduces weight. You now weigh `+ (this.weight--);
+        exercise(num){
+            return this.name + ` exercises ` + num + ` times! Exercise reduces weight. ` + this.name + ` now weighs `+ (this.weight-=(num*3)) + ` pounds!`;
         }
 
-        ageUp(){
-            return `Age: ` + (this.age++) + ` Height: ` + (this.height++) + ` Weight: ` + (this.weight++) + ` Mood: ` + (this.mood--) + ` Bank balance : $` + (this.bankAccount+10);  
+        ageUp(num){
+            this.weight += num*5;
+            this.mood -= num;
+            return this.name + ` has aged by ` + num + ` years to ` + (this.age+=num) + `! His bank account increases by $10 each Birthday. He now has $` + (this.bankAccount+=(num*10)) + `!`;  
         }
 
         buyHamster(hamster){
-        //hamster array 
+            this.hamsters.push(hamster);
+            return this.name + ` buys ` + hamster.nameofHamster + ` for $` + hamster.getPrice() + ` and his new balance is $` + (this.bankAccount-=hamster.getPrice()) + ` and his mood is now ` + (this.mood+=10) + `!`;
         }
-
 };
 
-let Timmy = new Person('Timmy',5,5.5,110,1,'',10);
-//name, age, height, weight, mood, hamsters, bankAccount
-//mood = bummed(1) to elated(5)
+let Timmy = new Person('Timmy',5,4.5,60,1,10);
+//name, age, height, weight, mood, bankAccount
+console.log(Timmy);
+console.log(Timmy.getAge());
 console.log(Timmy.eat(5));
+console.log(Timmy.exercise(5));
+console.log(Timmy.ageUp(4));
+let Gus = new Hamster();
+Gus.nameofHamster='Gus';
+console.log(Gus);
+Gus.nameOfOwner='Timmy';
+console.log(Timmy.buyHamster(Gus));
+console.log(Gus);
+console.log(Timmy.ageUp(6));
+console.log(Timmy.eat(2));
+console.log(Timmy.exercise(2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////
+/////Chef Make Dinners
+/////////////////////////////////////////////////////////////
+/// ex.
+//class Dinner {
+// }
+// class Chef {
+// }
+
+//Chef should be a factory of Dinner
+// Add a constructor to dinner that sets the string properties, appetizer, entree and dessert.
+// Add a method on chef that takes three arguments and returns a new Dinner based on those arguments.
+// Have the Chef create 3 dinners, log the dinners
+// 🔴 Hard Mode Save & Commit your work!
+// Your commit message should read something like:
+// "Dinner is served" Easy Mode Keep Going Save and Run Your Code
+/////......................................................................
+
